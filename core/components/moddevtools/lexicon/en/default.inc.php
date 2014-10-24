26cef5f4b1859787335711af0336799115d3f2bc
Kireev Vitaly <argnist88@gmail.com>
2014-10-24 17:11:21 +0800

    Лексиконы для таблички ресурсов в сниппете

--- core/components/moddevtools/lexicon/en/default.inc.php
+++ core/components/moddevtools/lexicon/en/default.inc.php
@@ -7,6 +7,7 @@ $_lang['moddevtools_snippets_intro'] = 'You can directly edit the snippets conta
 $_lang['moddevtools_templates_intro'] = 'You can directly edit the templates containing this chunk.';
 $_lang['moddevtools_res_template_intro'] = 'The table displays the resources with this template.';
 $_lang['moddevtools_res_chunk_intro'] = 'The table displays the resources that contain this chunk in the content or in the template.';
+$_lang['moddevtools_res_snippet_intro'] = 'The table displays the resources that contain this snippet in the content or in the template.';
 $_lang['moddevtools_grid_actions'] = 'Actions';
 $_lang['moddevtools_template_change'] = 'Change template';
 $_lang['moddevtools_search_desc'] = 'Search and replace strings in templates and chunks.';
