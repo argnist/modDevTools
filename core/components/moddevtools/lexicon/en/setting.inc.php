1444695cb9129a6dc7e276bcc5be839fffd9621d
Thomas Jakobi <thomas.jakobi@partout.info>
2021-12-16 11:42:40 +0100

    Normalize lexicon

--- core/components/moddevtools/lexicon/en/setting.inc.php
+++ core/components/moddevtools/lexicon/en/setting.inc.php
@@ -7,6 +7,6 @@
  */
 
 $_lang['setting_moddevtools.debug'] = 'Debug';
-$_lang['setting_moddevtools.debug_desc'] = 'Log debug informations in the MODX error log.';
+$_lang['setting_moddevtools.debug_desc'] = 'Log debug information in the MODX error log.';
 $_lang['setting_moddevtools.show_breadcrumb_context'] = 'Show Breadcrumb Context';
 $_lang['setting_moddevtools.show_breadcrumb_context_desc'] = 'Show the context key in the breadcrumb path.';
